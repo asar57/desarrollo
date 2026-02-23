9019a1db7f66d243dd720b210d5f81e0621d642e
andres <asaguirre57@ucatolica.edu.co>
2026-02-23 15:38:13 -0500

    Primer commit: programas de año bisiesto y fecha

---
+++ fecha.py
@@ -0,0 +1,8 @@
+from datetime import datetime, timedelta
+
+fecha = input("Ingrese una fecha (YYYY-MM-DD): ")
+fecha_obj = datetime.strptime(fecha, "%Y-%m-%d")
+
+nueva_fecha = fecha_obj + timedelta(days=1)
+
+print("Nueva fecha:", nueva_fecha.strftime("%Y-%m-%d"))
